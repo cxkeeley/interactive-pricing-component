348bc8c3d7c4de644c81a97e5bfc853715cf0f98
cxkeeley <cxkeeley@gmail.com>
2022-04-09 19:14:11 +0700

    commit

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
   <div>
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://twitter.com/bwalkermills" target="_blank" rel="noopener noreferrer">Byron</a>.
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>. Coded by <a href="https://twitter.com/cxkeeley" target="_blank" rel="noopener noreferrer">Darwin</a>.
   </div>
 </footer>
 
